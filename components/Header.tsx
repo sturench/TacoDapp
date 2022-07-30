ec177a86384e7948f20ce76247321c1aec6d06e9
Stuart Rench <sturench@gmail.com>
2022-07-30 16:23:13 -0500

    Some improvements

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import {FaTwitter, FaDiscord, FaShip, FaInfinity, FaEthereum} from 'react-icons/
 
 import ConnectButton from './ConnectButton';
 import Container from './Container';
+import Prose from './Prose';
 import NextLink from './NextLink';
 import projectConfig from '../config/projectConfig';
 import Logo from '../public/assets/logo.png';
@@ -18,89 +19,36 @@ export default function Header() {
     return (
         <div className="">
             <header className="py-2">
-                <Container>
-                    <div className="flex justify-between items-center">
-                        <div className="flex items-center space-x-2"></div>
-                        <div className="font-inter text-sm text-dark_choco font-black">
-                            <Link href={projectConfig.twitterUrl}
-                                  rel="noopener noreferrer"
-                                  target="_blank"
-                            >TWITTER</Link>
-                        </div>
+                <Prose>
+                    <div className="grid grid-cols-3  items-center border">
+                        <div className="col-start-2 flex items-center w-3/5 border">
+                            <div className="font-inter text-sm text-dark_choco font-black pr-10">
+                                <Link href={projectConfig.twitterUrl}
+                                      rel="noopener noreferrer"
+                                      target="_blank"
+                                >TWITTER</Link>
+                            </div>
 
-                        <div className="font-inter text-sm text-dark_choco font-black">
-                            <Link href={projectConfig.openseaCollectionUrl}
-                                  rel="noopener noreferrer"
-                                  target="_blank"
-                            >OPENSEA</Link>
+                            <div className="font-inter text-sm text-dark_choco font-black">
+                                <Link href={projectConfig.openseaCollectionUrl}
+                                      rel="noopener noreferrer"
+                                      target="_blank"
+                                >OPENSEA</Link>
+                            </div>
+                            <div className="font-inter text-sm text-dark_choco font-black pl-10">
+                                <Link href={projectConfig.scanUrl}
+                                      rel="noopener noreferrer"
+                                      target="_blank"
+                                >ETHERSCAN</Link>
+                            </div>
                         </div>
-                        <div className="font-inter text-sm text-dark_choco font-black">
-                            <Link href={projectConfig.scanUrl}
-                                  rel="noopener noreferrer"
-                                  target="_blank"
-                            >ETHERSCAN</Link>
-                        </div>
-                        {/*<NextLink href="/" className="text-2xl font-bold text-white">*/}
-                        {/*  <span className="flex items-center">*/}
-                        {/*    <Image*/}
-                        {/*      src={Logo}*/}
-                        {/*      alt={projectConfig.nftName}*/}
-                        {/*      width={35}*/}
-                        {/*      height={35}*/}
-                        {/*      className="rounded-full"*/}
-                        {/*    />*/}
-                        {/*    <span className="hidden sm:block ml-2">*/}
-                        {/*      {projectConfig.nftName}*/}
-                        {/*    </span>*/}
-                        {/*  </span>*/}
-                        {/*</NextLink>*/}
-
-                        <div className="flex items-center space-x-2 ml-2 sm:ml-0">
-                            {/*<ReactTooltip*/}
-                            {/*  id="header"*/}
-                            {/*  place="bottom"*/}
-                            {/*  type="dark"*/}
-                            {/*  effect="solid"*/}
-                            {/*  textColor="#e2e8f0"*/}
-                            {/*/>*/}
-                            {/*<a*/}
-                            {/*  href={projectConfig.scanUrl}*/}
-                            {/*  aria-label={`${projectConfig.nftName} on Etherscan`}*/}
-                            {/*  rel="noopener noreferrer"*/}
-                            {/*  target="_blank"*/}
-                            {/*  data-tip="Etherscan"*/}
-                            {/*  data-for="header"*/}
-                            {/*  className="bg-gray-700 hover:bg-gray-600 rounded-full p-2"*/}
-                            {/*>*/}
-                            {/*  <FaEthereum />*/}
-                            {/*</a>*/}
-                            {/*<a*/}
-                            {/*  href={projectConfig.twitterUrl}*/}
-                            {/*  aria-label={`${projectConfig.nftName} on Twitter`}*/}
-                            {/*  rel="noopener noreferrer"*/}
-                            {/*  target="_blank"*/}
-                            {/*  data-tip="Twitter"*/}
-                            {/*  data-for="header"*/}
-                            {/*  className="bg-gray-700 hover:bg-gray-600 rounded-full p-2"*/}
-                            {/*>*/}
-                            {/*  <FaTwitter />*/}
-                            {/*</a>*/}
-                            {/*<a*/}
-                            {/*  href={projectConfig.discordUrl}*/}
-                            {/*  aria-label={`${projectConfig.nftName} on Discord`}*/}
-                            {/*  rel="noopener noreferrer"*/}
-                            {/*  target="_blank"*/}
-                            {/*  data-tip="Discord"*/}
-                            {/*  data-for="header"*/}
-                            {/*  className="bg-gray-700 hover:bg-gray-600 rounded-full p-2"*/}
-                            {/*>*/}
-                            {/*  <FaDiscord />*/}
-                            {/*</a>*/}
-                            <ConnectButton/>
-
+                        <div className="col-start-3 justify-self-end w-1/5">
+                            <div className="flex items-center space-x-2 ml-2 sm:ml-0">
+                                <ConnectButton/>
+                            </div>
                         </div>
                     </div>
-                </Container>
+                </Prose>
             </header>
         </div>
     );
